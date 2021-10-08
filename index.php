919b68b2b31331640499e3a5667d6ad400f2a325
chen shu ting <a.0066715@gmail.com>
2021-10-08 14:05:37 +0800

    修改日期時間顯示區塊

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+use Carbon\Carbon;
 require __DIR__.'/bootstrap.php';
 require __DIR__.'/vendor/autoload.php';
 
@@ -82,7 +82,7 @@ try {
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">文章清單
-                    <small>{今日日期}</small>
+                    <small>{今日日期}<?php echo Carbon::now()->toDateString(); ?></small>
                 </h1>
             </div>
         </div>
@@ -102,7 +102,7 @@ try {
                 <p><?=mb_substr($row->content, 0, 130, "utf-8"),'…'?></p>
                 <p class="text-right">
                     <span class="glyphicon glyphicon-time"></span>
-                    發表於 <?=$row->created_at?>
+                    發表於 <?=$row->created_at?> <?=Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->diffForHumans()?>
                 </p>
             </div>
             <?php endwhile; ?>
